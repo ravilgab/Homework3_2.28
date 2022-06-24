4bb5beba1839d2a01ec2b2622fc9337ac8d1c0ad
Ravil <ravilgab@yahoo.com>
2022-06-24 21:56:21 +0300

    Project created

---
+++ Homework3_2.28/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework3_2.28
+//
+//  Created by Ravil on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
